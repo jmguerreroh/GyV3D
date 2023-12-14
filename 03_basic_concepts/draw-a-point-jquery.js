b7e9106a16022067eb0b6502e912228431b29c8f
Jose Miguel <josemiguel.guerrero@urjc.es>
2023-12-14 16:53:04 +0100

    Remove global variables, add comments, clean code, change license

--- 03_basic_concepts/draw-a-point-jquery.js
+++ 03_basic_concepts/draw-a-point-jquery.js
@@ -1,84 +1,90 @@
-    /**
-     * This function is used to initialize the scene
-     */
-    function init() {
-        // Get canvas object from the DOM
-        var canvas = document.getElementById("myCanvas");
+/**
+ * Copyright (c) 2023 Jose Miguel Guerrero Hernandez <josemiguel.guerrero@urjc.es>
+ * License: Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+ * http://creativecommons.org/licenses/by-sa/4.0/
+ * /
 
-        // Init WebGL context
-        var gl = canvas.getContext("webgl");
-        if (!gl) {
-            console.log("Failed to get the rendering context for WebGL");
-            return;
-        }
+/**
+ * This function is used to initialize the scene
+ */
+function init() {
+    // Get canvas object from the DOM
+    var canvas = document.getElementById("myCanvas");
 
-        // Read shaders using AJAX with jQuery
-        var vsSrc = document.getElementById('shaderVs').src;
-        $.get(vsSrc, function (vs) {
-            var fsSrc = document.getElementById('shaderFs').src;
-            $.get(fsSrc, function (fs) {
-                // Clear canvas
-                gl.clearColor(0.0, 0.0, 0.0, 1.0);
-                gl.clear(gl.COLOR_BUFFER_BIT);
+    // Init WebGL context
+    var gl = canvas.getContext("webgl");
+    if (!gl) {
+        console.log("Failed to get the rendering context for WebGL");
+        return;
+    }
 
-                // Init shaders		  
-                if (!initShaders(gl, vs, fs)) {
-                    console.log('Failed to intialize shaders.');
-                    return;
-                }
+    // Read shaders using AJAX with jQuery
+    var vsSrc = document.getElementById('shaderVs').src;
+    $.get(vsSrc, function (vs) {
+        var fsSrc = document.getElementById('shaderFs').src;
+        $.get(fsSrc, function (fs) {
+            // Clear canvas
+            gl.clearColor(0.0, 0.0, 0.0, 1.0);
+            gl.clear(gl.COLOR_BUFFER_BIT);
 
-                // Draw
-                gl.drawArrays(gl.POINTS, 0, 1);
-            });
+            // Init shaders		  
+            if (!initShaders(gl, vs, fs)) {
+                console.log('Failed to intialize shaders.');
+                return;
+            }
+
+            // Draw
+            gl.drawArrays(gl.POINTS, 0, 1);
         });
+    });
 
+}
+
+/**
+ * This function is used to initialize the shaders
+ * @param {WebGLRenderingContext} gl 
+ * @param {String} vs_source 
+ * @param {String} fs_source 
+ * @returns {Boolean}
+ */
+function initShaders(gl, vs_source, fs_source) {
+    // Compile shaders
+    var vertexShader = makeShader(gl, vs_source, gl.VERTEX_SHADER);
+    var fragmentShader = makeShader(gl, fs_source, gl.FRAGMENT_SHADER);
+
+    // Create program
+    var glProgram = gl.createProgram();
+
+    // Attach and link shaders to the program
+    gl.attachShader(glProgram, vertexShader);
+    gl.attachShader(glProgram, fragmentShader);
+    gl.linkProgram(glProgram);
+    if (!gl.getProgramParameter(glProgram, gl.LINK_STATUS)) {
+        alert("Unable to initialize the shader program");
+        return false;
     }
 
-    /**
-     * This function is used to initialize the shaders
-     * @param {WebGLRenderingContext} gl 
-     * @param {String} vs_source 
-     * @param {String} fs_source 
-     * @returns {Boolean}
-     */
-    function initShaders(gl, vs_source, fs_source) {
-        // Compile shaders
-        var vertexShader = makeShader(gl, vs_source, gl.VERTEX_SHADER);
-        var fragmentShader = makeShader(gl, fs_source, gl.FRAGMENT_SHADER);
+    // Use program
+    gl.useProgram(glProgram);
+    gl.program = glProgram;
 
-        // Create program
-        var glProgram = gl.createProgram();
+    return true;
+}
 
-        // Attach and link shaders to the program
-        gl.attachShader(glProgram, vertexShader);
-        gl.attachShader(glProgram, fragmentShader);
-        gl.linkProgram(glProgram);
-        if (!gl.getProgramParameter(glProgram, gl.LINK_STATUS)) {
-            alert("Unable to initialize the shader program");
-            return false;
-        }
-
-        // Use program
-        gl.useProgram(glProgram);
-        gl.program = glProgram;
-
-        return true;
+/**
+ * This function is used to create a shader
+ * @param {WebGLRenderingContext} gl 
+ * @param {String} src 
+ * @param {Number} type 
+ * @returns {WebGLShader}
+ */
+function makeShader(gl, src, type) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, src);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert("Error compiling shader: " + gl.getShaderInfoLog(shader));
+        return;
     }
-
-    /**
-     * This function is used to create a shader
-     * @param {WebGLRenderingContext} gl 
-     * @param {String} src 
-     * @param {Number} type 
-     * @returns {WebGLShader}
-     */
-    function makeShader(gl, src, type) {
-        var shader = gl.createShader(type);
-        gl.shaderSource(shader, src);
-        gl.compileShader(shader);
-        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-            alert("Error compiling shader: " + gl.getShaderInfoLog(shader));
-            return;
-        }
-        return shader;
-    }
+    return shader;
+}
--- 03_basic_concepts/draw-a-point-vanilla.js
+++ 03_basic_concepts/draw-a-point-vanilla.js
@@ -1,99 +1,105 @@
-    /**
-     * This function is used to initialize the scene
-     */
-    function init() {
-        // Get canvas object from the DOM
-        var canvas = document.getElementById("myCanvas");
+/**
+ * Copyright (c) 2023 Jose Miguel Guerrero Hernandez <josemiguel.guerrero@urjc.es>
+ * License: Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+ * http://creativecommons.org/licenses/by-sa/4.0/
+ * /
 
-        // Init WebGL context
-        var gl = canvas.getContext("webgl");
-        if (!gl) {
-            console.log("Failed to get the rendering context for WebGL");
-            return;
-        }
+/**
+ * This function is used to initialize the scene
+ */
+function init() {
+    // Get canvas object from the DOM
+    var canvas = document.getElementById("myCanvas");
 
-        // Read shaders using AJAX
-        var vsSrc = document.getElementById('shaderVs').src;
-        readScriptContent(vsSrc, function (vs) {
-            var fsSrc = document.getElementById('shaderFs').src;
-            readScriptContent(fsSrc, function (fs) {
-                // Clear canvas
-                gl.clearColor(0.0, 0.0, 0.0, 1.0);
-                gl.clear(gl.COLOR_BUFFER_BIT);
-
-                // Init shaders		  
-                if (!initShaders(gl, vs, fs)) {
-                    console.log('Failed to intialize shaders.');
-                    return;
-                }
-
-                // Draw
-                gl.drawArrays(gl.POINTS, 0, 1);
-            });
-        });
+    // Init WebGL context
+    var gl = canvas.getContext("webgl");
+    if (!gl) {
+        console.log("Failed to get the rendering context for WebGL");
+        return;
     }
 
-    /**
-     * This function is used to read the content of a script
-     * @param {String} scriptSrc 
-     * @param {Function} callback 
-     */
-    function readScriptContent(scriptSrc, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", scriptSrc)
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                callback(xhr.responseText);
+    // Read shaders using AJAX
+    var vsSrc = document.getElementById('shaderVs').src;
+    readScriptContent(vsSrc, function (vs) {
+        var fsSrc = document.getElementById('shaderFs').src;
+        readScriptContent(fsSrc, function (fs) {
+            // Clear canvas
+            gl.clearColor(0.0, 0.0, 0.0, 1.0);
+            gl.clear(gl.COLOR_BUFFER_BIT);
+
+            // Init shaders		  
+            if (!initShaders(gl, vs, fs)) {
+                console.log('Failed to intialize shaders.');
+                return;
             }
-        };
-        xhr.send();
-    }
 
-    /**
-     * This function is used to initialize the shaders
-     * @param {WebGLRenderingContext} gl 
-     * @param {String} vs_source 
-     * @param {String} fs_source 
-     * @returns {Boolean}
-     */
-    function initShaders(gl, vs_source, fs_source) {
-        // Compile shaders
-        var vertexShader = makeShader(gl, vs_source, gl.VERTEX_SHADER);
-        var fragmentShader = makeShader(gl, fs_source, gl.FRAGMENT_SHADER);
+            // Draw
+            gl.drawArrays(gl.POINTS, 0, 1);
+        });
+    });
+}
 
-        // Create program
-        var glProgram = gl.createProgram();
-
-        // Attach and link shaders to the program
-        gl.attachShader(glProgram, vertexShader);
-        gl.attachShader(glProgram, fragmentShader);
-        gl.linkProgram(glProgram);
-        if (!gl.getProgramParameter(glProgram, gl.LINK_STATUS)) {
-            alert("Unable to initialize the shader program");
-            return false;
+/**
+ * This function is used to read the content of a script
+ * @param {String} scriptSrc 
+ * @param {Function} callback 
+ */
+function readScriptContent(scriptSrc, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", scriptSrc)
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            callback(xhr.responseText);
         }
+    };
+    xhr.send();
+}
 
-        // Use program
-        gl.useProgram(glProgram);
-        gl.program = glProgram;
+/**
+ * This function is used to initialize the shaders
+ * @param {WebGLRenderingContext} gl 
+ * @param {String} vs_source 
+ * @param {String} fs_source 
+ * @returns {Boolean}
+ */
+function initShaders(gl, vs_source, fs_source) {
+    // Compile shaders
+    var vertexShader = makeShader(gl, vs_source, gl.VERTEX_SHADER);
+    var fragmentShader = makeShader(gl, fs_source, gl.FRAGMENT_SHADER);
 
-        return true;
+    // Create program
+    var glProgram = gl.createProgram();
+
+    // Attach and link shaders to the program
+    gl.attachShader(glProgram, vertexShader);
+    gl.attachShader(glProgram, fragmentShader);
+    gl.linkProgram(glProgram);
+    if (!gl.getProgramParameter(glProgram, gl.LINK_STATUS)) {
+        alert("Unable to initialize the shader program");
+        return false;
     }
 
-    /**
-     * This function is used to create a shader
-     * @param {WebGLRenderingContext} gl 
-     * @param {String} src 
-     * @param {Number} type 
-     * @returns {WebGLShader}
-     */
-    function makeShader(gl, src, type) {
-        var shader = gl.createShader(type);
-        gl.shaderSource(shader, src);
-        gl.compileShader(shader);
-        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-            alert("Error compiling shader: " + gl.getShaderInfoLog(shader));
-            return;
-        }
-        return shader;
+    // Use program
+    gl.useProgram(glProgram);
+    gl.program = glProgram;
+
+    return true;
+}
+
+/**
+ * This function is used to create a shader
+ * @param {WebGLRenderingContext} gl 
+ * @param {String} src 
+ * @param {Number} type 
+ * @returns {WebGLShader}
+ */
+function makeShader(gl, src, type) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, src);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert("Error compiling shader: " + gl.getShaderInfoLog(shader));
+        return;
     }
+    return shader;
+}
